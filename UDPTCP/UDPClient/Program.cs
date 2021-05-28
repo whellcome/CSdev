a7814b0ad9def48d4a9de9ad85a594ce5d0b3601
whellcome <whellcome@narod.ru>
2021-05-28 17:41:18 +0200

    UDP services added

---
+++ UDPTCP/UDPClient/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            int port = 8082;
+            var udpEndPoint = new IPEndPoint(ip, port);
+            var udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            udpSocket.Bind(udpEndPoint);
+            while (true)
+            {
+                Console.Write("Enter your message: ");
+                var message = Console.ReadLine();
+                var recieverEndPoint = new IPEndPoint(ip, 8081);
+                udpSocket.SendTo(Encoding.UTF8.GetBytes(message), recieverEndPoint);
+                if (message.Equals("exit")) break;
+                
+                var buffer = new byte[256];
+                var data = new StringBuilder();
+                EndPoint senderEndPoint = new IPEndPoint(IPAddress.Any, recieverEndPoint.Port);
+                do
+                {
+                    udpSocket.ReceiveFrom(buffer, ref senderEndPoint);
+                    data.Append(Encoding.UTF8.GetString(buffer));
+                }
+                while (udpSocket.Available > 0);
+
+                Console.WriteLine(data);
+            }
+        }
+    }
+}
---
+++ UDPTCP/UDPServer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            int port = 8081;
+            var udpEndPoint = new IPEndPoint(ip, port);
+            var udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            udpSocket.Bind(udpEndPoint);
+            while (true)
+            {
+                var buffer = new byte[256];
+                var data = new StringBuilder();
+                EndPoint senderEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                do
+                {
+                    udpSocket.ReceiveFrom(buffer, ref senderEndPoint);
+                    data.Append(Encoding.UTF8.GetString(buffer));
+                }
+                while (udpSocket.Available > 0);
+
+                Console.WriteLine(data);
+                udpSocket.SendTo(Encoding.UTF8.GetBytes("Success"), senderEndPoint);
+                
+                if (data.ToString().Equals("exit", StringComparison.InvariantCultureIgnoreCase))
+                    break;
+            }
+        }
+    }
+}
