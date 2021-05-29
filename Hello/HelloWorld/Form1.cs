9086c34186876286df140485d3ad8f1a2dcf6949
whellcome <whellcome@narod.ru>
2021-05-29 22:54:04 +0200

    HelloWorld update (volume mute etc.)

--- Hello/HelloWorld/Form1.cs
+++ Hello/HelloWorld/Form1.cs
@@ -1,15 +1,23 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace HelloWorld
 {
     public partial class Form1 : Form
     {
+        private const int APPCOMMAND_VOLUME_MUTE = 0x80000;
+        private const int APPCOMMAND_VOLUME_UP = 0xA0000;
+        private const int APPCOMMAND_VOLUME_DOWN = 0x90000;
+        private const int WM_APPCOMMAND = 0x319;        
+        [DllImport("user32.dll")]
+        private static extern IntPtr SendMessageW(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
         public Form1()
         {
             InitializeComponent();
             this.FormBorderStyle = FormBorderStyle.None;
             this.WindowState = FormWindowState.Maximized;
+            Cursor.Hide();
             timer1.Start();
         }
         private void Form1_Load(object sender, EventArgs e)
@@ -19,20 +27,19 @@ namespace HelloWorld
         private void timer1_Tick(object sender, EventArgs e)
         {
             timer1.Stop();
-            
+            SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle, (IntPtr)APPCOMMAND_VOLUME_MUTE);
             DisplayHi(this, this.label1, "Wake up, Neo...");
             DisplayHi(this, this.label1, "The Matrix has you...");
             DisplayHi(this, this.label1, "Follow the white rabbit.");
             DisplayHi(this, this.label1, "Knock, knock, Neo.");
-
+            SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle, (IntPtr)APPCOMMAND_VOLUME_UP);
+            SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle, (IntPtr)APPCOMMAND_VOLUME_DOWN);
             this.Close();
-            
         }
-        internal static void DisplayHi(Form form, Label label, string hi)
+        private static void DisplayHi(Form form, Label label, string hi)
         {
             label.Text = "";
             form.Refresh();
-
             for (int i = 0; i < 1; i++)
             {
                 label.Text += "_";
--- Hello/HelloWorld/Program.cs
+++ Hello/HelloWorld/Program.cs
@@ -1,13 +1,11 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace HelloWorld
 {
     static class Program
     {
+        
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
