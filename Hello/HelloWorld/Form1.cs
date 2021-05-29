ac1b3bb7016974f66c80e013933775298e9b4188
whellcome <whellcome@narod.ru>
2021-05-29 23:42:22 +0200

    HelloWorld update

--- Hello/HelloWorld/Form1.cs
+++ Hello/HelloWorld/Form1.cs
@@ -28,6 +28,15 @@ namespace HelloWorld
         {
             timer1.Stop();
             SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle, (IntPtr)APPCOMMAND_VOLUME_MUTE);
+            for (int i = 0; i < 3; i++)
+            {
+                this.label1.Text = "_";
+                this.Refresh();
+                System.Threading.Thread.Sleep(500);
+                this.label1.Text = "";
+                this.Refresh();
+                System.Threading.Thread.Sleep(500);
+            };
             DisplayHi(this, this.label1, "Wake up, Neo...");
             DisplayHi(this, this.label1, "The Matrix has you...");
             DisplayHi(this, this.label1, "Follow the white rabbit.");
@@ -38,14 +47,12 @@ namespace HelloWorld
         }
         private static void DisplayHi(Form form, Label label, string hi)
         {
-            label.Text = "";
-            form.Refresh();
             for (int i = 0; i < 1; i++)
             {
-                label.Text += "_";
+                label.Text = "_";
                 form.Refresh();
                 System.Threading.Thread.Sleep(250);
-                label.Text = label.Text.Remove(label.Text.Length - 1);
+                label.Text = "";
                 form.Refresh();
             };
             char[] hiChar = hi.ToCharArray();
